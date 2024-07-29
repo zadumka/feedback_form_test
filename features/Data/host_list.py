cd96b9ddf3a0e11b1c73060732ac734f0411c81e
zadumka <alenahas1987@gmail.com>
2024-07-29 20:23:25 +0300

    coment step

--- features/Data/host_list.py
+++ features/Data/host_list.py
@@ -4,6 +4,7 @@ HOST_LIST = {
         "host3": "cs-direct-lp-ua.goit.global",
         "host4": "cs-m-lp-ua.goit.global",
         "host5": "neo.goit.global",
-        "host6": "goit.global/ro/courses/fullstack"
+        "host6": "goit.global/ro/courses/fullstack",
+        "host7": "neobachelor-direct-lp-ua.goit.global/"
 
 }
